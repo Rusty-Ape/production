cb8ad1d58cae65cf79132d9b040be0879ca16e8f
Aise <goodluck.arasomwan@gmail.com>
2023-02-05 01:18:19 +0100

    Production

---
+++ _next/static/R4F_eZuc4VWqCVYaJhRcO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/aef0e399aab5bf4b.css","static/chunks/pages/index-6ada0ff92ac9bc56.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
